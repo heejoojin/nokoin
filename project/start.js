2d99f7b923a592f2070abb756af02e3159ca1237
heejoojin <hjin77@gatech.edu>
2021-10-23 15:41:01 -0400

    update files

--- project/start.js
+++ project/start.js
@@ -1,4 +1,9 @@
 var express = require('express');
+
+// communication between javascript and js files 
+// const { spawn } = require('child_process');
+// const childPython = spawn('python', ['etherum.py']);
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -14,10 +19,7 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.get('/loading', function(request, response) {
-  response.render('pages/loading');
-});
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
