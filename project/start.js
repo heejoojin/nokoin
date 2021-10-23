3f25732000c6fd90e22db7a06b6928673a95cc18
heejoojin <hjin77@gatech.edu>
2021-10-22 21:52:00 -0400

    Update

--- project/start.js
+++ project/start.js
@@ -14,26 +14,6 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.get('/about', function(request, response) {
-  response.render('pages/about');
-});
-
-app.get('/class', function(request, response) {
-  response.render('pages/class');
-});
-
-app.get('/contact', function(request, response) {
-  response.render('pages/contact');
-});
-
-app.get('/location', function(request, response) {
-  response.render('pages/location');
-});
-
-app.get('/cart', function(request, response) {
-  response.render('pages/cart');
-});
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
