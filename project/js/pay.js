776110774706f0aa7a6f5bb08f5eef98c4b127ec
heejoojin <hjin77@gatech.edu>
2021-10-23 14:21:56 -0400

    qrcode reader

--- project/js/pay.js
+++ project/js/pay.js
@@ -1,6 +1,39 @@
 
+Dynamsoft.DBR.BarcodeScanner.organizationID = "100731799";
+Dynamsoft.DBR.BarcodeReader.productKeys = "t0068NQAAAEa2vhEKG8ZfiSEzbWTHMdB3k8iXEU65DKVd3BncYIPvJPlni465ybIveVFFFdg3qugMursl41wKsk9q1waDWJQ=";
+let pScanner = null;
+document.getElementById('button').onclick = async function () {
+    try {
+        let scanner = await (pScanner = pScanner || Dynamsoft.DBR.BarcodeScanner.createInstance());
+        /* 
+         * onFrameRead is triggered once each frame is read. 
+         * There can be one or multiple barcodes on each frame.
+         */
+        await scanner.setUIElement(document.getElementById('video'));
+        scanner.onFrameRead = results => {
+            console.log("Barcodes on one frame:");
+            for (let result of results) {
+                console.log(result.barcodeFormatString + ": " + result.barcodeText);
+            }
+        };
 
-function takeQRcode() {
+        /* 
+         * onUnduplicatdRead is triggered once a new barcode is found. 
+         * The amount of time that the library 'remembers' the found barcode is defined by duplicateForgetTime 
+         * in the ScanSettings interface of the BarcodeScanner class. By default that is set to 3000 ms (or 3 secs) 
+         */
+        scanner.onUnduplicatedRead = (txt, result) => {
+            alert(txt);
+            console.log("Unique Code Found: " + result);
+        }
+        await scanner.show();
+    } catch (ex) {
+        alert(ex.message);
+        throw ex;
+    }
+};
+
+// function takeQRcode() {
     
     // var video = document.querySelector("#video");
     // if (navigator.mediaDevices.getUserMedia) {
@@ -12,86 +45,19 @@ function takeQRcode() {
     //       console.log("Something went wrong!");
     //     });
     // }
+    // 
 
-    let pScanner = null;
-    (async () => {
-      let scanner = await (pScanner = pScanner || Dynamsoft.DBR.BarcodeScanner.createInstance());
-      scanner.onFrameRead = results => {if (results.length > 0) console.log(results)};
-      scanner.onUnduplicatedRead = (txt, result) => {alert(txt);};
-      await scanner.show();
-    })();
-    // var scanner = document.querySelector("#video");
-    // let scanner = null;
-    //     (async ()=>{
-    //         scanner = await Dynamsoft.DBR.BarcodeScanner.createInstance();
-    //         await scanner.setUIElement(document.getElementById('video'));
-    //         scanner.onFrameRead = results => {console.log(results);};
-    //         scanner.onUnduplicatedRead = (txt, result) => {alert(txt);};
-
-    //         await scanner.updateVideoSettings({ video: { width: 400, height: 300, facingMode: "environment" } });
-    //         await scanner.updateRuntimeSettings("speed"); // speed, balance, coverage
-    //         let runtimeSettings = await scanner.getRuntimeSettings(); // get settings for modification
-    //         // Specify which symbologies are to enabled
-    //         runtimeSettings.barcodeFormatIds = Dynamsoft.DBR.EnumBarcodeFormat.BF_ONED | Dynamsoft.DBR.EnumBarcodeFormat.BF_QR_CODE;
-    //         await scanner.updateRuntimeSettings(runtimeSettings);
-    //         let scanSettings = await scanner.getScanSettings();
-    //         // Disregard duplicated results found in a specified time period
-    //         scanSettings.duplicateForgetTime = 20000;
-    //         // Set a interval so that the CPU can relax
-    //         scanSettings.intervalTime = 300;
-    //         await scanner.updateScanSettings(scanSettings);
-    //         await scanner.show();
-
-    //         // scanner = await Dynamsoft.DBR.BarcodeScanner.createInstance();
-    //         // scanner.onFrameRead = results => {console.log(results);};
-    //         // scanner.onUnduplicatedRead = (txt, result) => {alert(txt);};
-    //         // document.getElementById('video').appendChild(scanner.getUIElement());
-    //         // await scanner.show();
-    //     })();
-}
-
-// var resultContainer = document.getElementById('qr-reader-results');
-// var lastResult, countResults = 0;
-
-// function onScanSuccess(decodedText, decodedResult) {
-//     if (decodedText !== lastResult) {
-//         ++countResults;
-//         lastResult = decodedText;
-//         // Handle on success condition with the decoded message.
-//         console.log(`Scan result ${decodedText}`, decodedResult);
-//     }
-// }
-
-// var html5QrcodeScanner = new Html5QrcodeScanner(
-//     "qr-reader", { fps: 10, qrbox: 250 });
-// html5QrcodeScanner.render(onScanSuccess);
-
-// paypal.Buttons({
-//     style: {
-//         layout: 'vertical',
-//         color:  'black',
-//         shape:  'pill',
-//         label:  'checkout',
-//         tagline: 'false',
-//         size: 'responsive'
-//     },
     
-//     createOrder:function(data, actions) {
-//         return actions.order.create({
-//             purchase_units: [{
-//               amount: {
-//                 value: $('#paypal-price').text()
-//               }
-//             }]
-//           });
-//     },
+    
 
-//     onApprove:function(data, actions) {
-//         return actions.order.capture().then(function(detail) {
-//             console.log(detail);
-//         });
-//     }
-// }).render('#paypal-button');
-
-
-
+    // let scanner = null;
+    // (async () => {
+    //   let scanner = await Dynamsoft.DBR.BarcodeScanner.createInstance();
+    //   await scanner.setUIElement(document.getElementById('video'));
+    //   // await scanner.setResolution(400, 300);
+    //   scanner.onFrameRead = results => {if (results.length > 0) console.log(results)};
+    //   // txt = user id
+    //   scanner.onUnduplicatedRead = (txt, result) => {alert(txt);};
+    //   await scanner.show();
+    // })();
+// }
