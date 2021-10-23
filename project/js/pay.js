6d90f71b7638fb00fe3bc1d3bcb4d705e7902625
heejoojin <hjin77@gatech.edu>
2021-10-23 12:55:23 -0400

    Access camera

--- project/js/pay.js
+++ project/js/pay.js
@@ -1,33 +1,45 @@
 
-setTimeout(function() {
-    paypal.Buttons({
-        style: {
-            layout: 'vertical',
-            color:  'black',
-            shape:  'pill',
-            label:  'checkout',
-            tagline: 'false',
-            size: 'responsive'
-        },
-        
-        createOrder:function(data, actions) {
-            return actions.order.create({
-                purchase_units: [{
-                  amount: {
-                    value: $('#paypal-price').text()
-                  }
-                }]
-              });
-        },
 
-        onApprove:function(data, actions) {
-            return actions.order.capture().then(function(detail) {
-                console.log(detail);
-                // window.location.href = "/";
-            });
-        }
-    }).render('#paypal-button');
-}, 1000);
+function takeQRcode() {
+    
+    var video = document.querySelector("#video");
+    if (navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then(function (stream) {
+          video.srcObject = stream;
+        })
+        .catch(function (err0r) {
+          console.log("Something went wrong!");
+        });
+    }
+}
+
+// paypal.Buttons({
+//     style: {
+//         layout: 'vertical',
+//         color:  'black',
+//         shape:  'pill',
+//         label:  'checkout',
+//         tagline: 'false',
+//         size: 'responsive'
+//     },
+    
+//     createOrder:function(data, actions) {
+//         return actions.order.create({
+//             purchase_units: [{
+//               amount: {
+//                 value: $('#paypal-price').text()
+//               }
+//             }]
+//           });
+//     },
+
+//     onApprove:function(data, actions) {
+//         return actions.order.capture().then(function(detail) {
+//             console.log(detail);
+//         });
+//     }
+// }).render('#paypal-button');
 
 
 
